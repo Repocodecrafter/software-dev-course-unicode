6aa4743b691d6ebd5e2d66b44f3fbe3697eb049e
meghcodes <marellameghana99@gmail.com>
2026-04-15 23:21:26 -0500

    Complete string Unicode and character manipulation tasks

--- unicode.js
+++ unicode.js
@@ -86,15 +86,25 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0); // 'C = 67
+let thirdCodePoint = inputString1.charCodeAt(2); // 'd' = 100
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72,101,108,108); // converting code points to characters
+
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let firstCharCode = inputString2.charCodeAt(0);  // 'L' = 76
+let lastCharCode = inputString2.charCodeAt(inputString2.length - 1); // 'h' = 104
+
+let firstChar = String.fromCharCode(firstCharCode);
+let lastChar = String.fromCharCode(lastCharCode);
+
+let middlePart = inputString2.slice(1, inputString2.length -1);  // 'aunc'
+
+let swappedString = lastChar + middlePart + firstChar; // 'hauncL'
+
 
 // Log all results
 console.log({
